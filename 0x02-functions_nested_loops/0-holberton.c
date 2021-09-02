0a87356f278d65606186b9a8c5763b3a1ee58470
mwanjiku56 <mwanjiku56@gmail.com>
2021-09-02 13:21:55 +0300

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * prints putchar
+ * @return 0
+ */
+int main(void)
+{
+    char school[] = "_putchar";
+    int i = 0;
+    while (school[i] != '\0')
+        {
+            _putchar(school[i]);
+            i++;
+        }
+_   _putchar('\n');
+    return (0);
+}
