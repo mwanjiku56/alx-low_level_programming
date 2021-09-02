7aae8eba33bf7a17b2ddad251f7549b95e870eb2
mwanjiku56 <mwanjiku56@gmail.com>
2021-09-02 22:57:19 +0300

    new update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+
+#include "main.h"
 
 /**
  * mul - function that multiplies two integers.
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_numbers - function that prints the numbers, from 0 to 9.
  *
  * Return: nothing.
+ *
  */
 void print_numbers(void)
 {
