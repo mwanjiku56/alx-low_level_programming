c8026a6920b0ee161637c16fa5bf40f76d1bb84e
mwanjiku56 <mwanjiku56@gmail.com>
2021-10-01 23:50:39 +0300

    adding new prototype to the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,8 @@
 #ifndef VARFUNC_H
 #define VARFUNC_H
-#include <stdarg.h>
+int _putchar(char c)
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct type - Struct data type
- *
- * @op: data type argument
- * @f: The function associated
- */
-
-typedef struct type
-{
-	char *op;
-	void (*f)(va_list, char *);
-} type_t;
-
 #endif
