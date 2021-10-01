191e784dd542320dff9f1da96c23dff2893ff021
mwanjiku56 <mwanjiku56@gmail.com>
2021-10-01 23:54:16 +0300

    edited the header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,7 @@
-#ifndef VARFUNC_H
-#define VARFUNC_H
-int _putchar(char c)
+#ifndef MAIN_H
+#define MAIN_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif
+#endif /* #ifndef MAIN_H */
